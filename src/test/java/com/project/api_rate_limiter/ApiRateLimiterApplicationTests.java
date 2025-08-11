5c726fbec5b35e83518e591c3ab9b81a318f3b95
vaninx <vaninwabuoku@gmail.com>
2025-08-11 12:37:26 +0100

    Spring boot setup with /health endpoint

---
+++ src/test/java/com/project/api_rate_limiter/ApiRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api_rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
